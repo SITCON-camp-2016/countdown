06318fa2d341af5f707e80f54cc75851f0e7e395
Elantris <j88107333@gmail.com>
2016-08-04 19:05:46 +0800

    countDiff: change to json format

--- src/scripts/util/countDiff.jsx
+++ src/scripts/util/countDiff.jsx
@@ -2,20 +2,20 @@ var moment = require('moment');
 
 var end = parseInt(moment('2016-08-04 22').format('X'));
 
-function pad(x) {
-	if (x < 10) return '0' + x;
-	else return x;
-}
-
 function countDiff() {
 	var diff = end - parseInt(moment().format('X'));
 	var duration = moment.duration(diff, 'seconds');
-	return pad(duration.hours()) + ':' + pad(duration.minutes()) + ':' + pad(duration.seconds());
+	return {
+		h: duration.hours(),
+		m: duration.minutes(),
+		s: duration.seconds()
+	};
 }
 
 // test
-// setInterval(function() {
-// 	console.log(countDiff());
-// }, 1000);
+
+setInterval(function() {
+	console.log(countDiff());
+}, 1000);
 
 module.exports = countDiff;
